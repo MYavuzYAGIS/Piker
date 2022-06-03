cd988d048fb73ad85a96219796bf0bc9186f2b9f
Yavuz <m.yavuzyagis@gmail.com>
2022-06-03 20:20:31 +0300

    Fix some bugs and add some features. This is an AUTOMATED Commit

--- main.go
+++ main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"github.com/fatih/color"
+	"os"
 )
+
+//base64
+//md5
+//sha1
 
 var (
 	base64Success = color.New(color.FgBlue)
@@ -29,11 +35,28 @@ func spawn() {
 		h += "Piker [OPTIONS] [FILE/URL|-]    \n\n  "
 
 		h += "Options:\n"
-		h += "-o, --online	Use the online endpoint to use readied API \n"
-		h += "-f, --offline 	Use a custom file to search for the results \n"
-		h += "-be  --base64 encode the given string\n"
-		h += "-bd  --base64 decode the given string\n"
-		h += "
+		h += "-o, 	--online	Use the online endpoint to use readied API \n"
+		h += "-f, 	--offline 	Use a custom file to search for the results \n"
+		h += "-be,  --base64 encode the given string\n"
+		h += "-bd,  --base64 decode the given string\n"
+		h += "-m,   --md5	reverse lookup for  for md5 hash\n"
+		h += "-s,   --sha1  reverse lookup for for sha1 hash values\n"
+		h += "-l, 	--list  custom dict that contains API endpoints to hit.\n"
+
+		h += "Exit Codes:\n"
+		h += fmt.Sprintf("  %d\t%s\n", exitOK, "OK")
+		h += fmt.Sprintf("  %d\t%s\n", exitOpenFile, "Failed to open file")
+		h += fmt.Sprintf("  %d\t%s\n", exitReadInput, "Failed to read input")
+		h += fmt.Sprintf("  %d\t%s\n", exitFetchURL, "Failed to fetch URL")
+		h += fmt.Sprintf("  %d\t%s\n", exitParseStatements, "Failed to parse statements")
+		h += "\n"
+
+		h += "Examples:\n"
+		h += "  Piker -f -s /tmp/apiresponse.pk\n"
+		h += "  Piker -o -l listfile filetocrack.pk\n"
+		h += "  Piker -f -m listfile ec55d3e698d289f2afd663725127bace "
+
+		fmt.Fprintf(os.Stderr, h)
 
 	}
 }
