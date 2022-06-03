a4397a2b29eeab7d2fa67dbc885b1a213247017a
Yavuz <m.yavuzyagis@gmail.com>
2022-06-04 01:34:27 +0300

    finish flags and options

---
+++ Readme.md
@@ -0,0 +1,4 @@
+This is basically a Swiss Army Knife for reverse-lookups for Sha and Md5 hashes.
+Also encoder/decoder for base64 format.
+
+Intended to be architectured t work in online mode and in offline mode.
--- main.go
+++ main.go
@@ -27,6 +27,8 @@ const (
 	exitParseStatements
 )
 
+var PikerVersion = "dev"
+
 func spawn() {
 	flag.Usage = func() {
 
@@ -37,11 +39,11 @@ func spawn() {
 		h += "Options:\n"
 		h += "-o, 	--online	Use the online endpoint to use readied API \n"
 		h += "-f, 	--offline 	Use a custom file to search for the results \n"
-		h += "-be,  --base64 encode the given string\n"
-		h += "-bd,  --base64 decode the given string\n"
-		h += "-m,   --md5	reverse lookup for  for md5 hash\n"
-		h += "-s,   --sha1  reverse lookup for for sha1 hash values\n"
-		h += "-l, 	--list  custom dict that contains API endpoints to hit.\n"
+		h += "-be,  --encode 	encode the given string\n"
+		h += "-bd,  --decode 	decode the given string\n"
+		h += "-m,   --md5		reverse lookup for  for md5 hash\n"
+		h += "-s,   --sha1  	reverse lookup for for sha1 hash values\n"
+		h += "-l, 	--list  	custom dict that contains API endpoints to hit.\n"
 
 		h += "Exit Codes:\n"
 		h += fmt.Sprintf("  %d\t%s\n", exitOK, "OK")
@@ -63,35 +65,35 @@ func spawn() {
 
 func main() {
 	var (
-		ungronFlag     bool
-		colorizeFlag   bool
-		monochromeFlag bool
-		streamFlag     bool
-		noSortFlag     bool
-		versionFlag    bool
-		insecureFlag   bool
-		jsonFlag       bool
-		valuesFlag     bool
+		onlineFlag     bool
+		offlineFlag    bool
+		baseDecodeFlag bool
+		baseEncodeFlag bool
+		md5Flag        bool
+		sha1Flag       bool
+		listFlag       bool
 	)
 
-	flag.BoolVar(&ungronFlag, "ungron", false, "")
-	flag.BoolVar(&ungronFlag, "u", false, "")
-	flag.BoolVar(&colorizeFlag, "colorize", false, "")
-	flag.BoolVar(&colorizeFlag, "c", false, "")
-	flag.BoolVar(&monochromeFlag, "monochrome", false, "")
-	flag.BoolVar(&monochromeFlag, "m", false, "")
-	flag.BoolVar(&streamFlag, "s", false, "")
-	flag.BoolVar(&streamFlag, "stream", false, "")
-	flag.BoolVar(&noSortFlag, "no-sort", false, "")
-	flag.BoolVar(&versionFlag, "version", false, "")
-	flag.BoolVar(&insecureFlag, "k", false, "")
-	flag.BoolVar(&insecureFlag, "insecure", false, "")
-	flag.BoolVar(&jsonFlag, "j", false, "")
-	flag.BoolVar(&jsonFlag, "json", false, "")
-	flag.BoolVar(&valuesFlag, "values", false, "")
-	flag.BoolVar(&valuesFlag, "value", false, "")
-	flag.BoolVar(&valuesFlag, "v", false, "")
+	flag.BoolVar(&onlineFlag, "online", false, "")
+	flag.BoolVar(&onlineFlag, "o", false, "")
+	flag.BoolVar(&offlineFlag, "offline", false, "")
+	flag.BoolVar(&offlineFlag, "f", false, "")
+	flag.BoolVar(&baseDecodeFlag, "bd", false, "")
+	flag.BoolVar(&baseDecodeFlag, "decode", false, "")
+	flag.BoolVar(&baseEncodeFlag, "be", false, "")
+	flag.BoolVar(&baseEncodeFlag, "encode", false, "")
+	flag.BoolVar(&md5Flag, "m", false, "")
+	flag.BoolVar(&md5Flag, "md5", false, "")
+	flag.BoolVar(&sha1Flag, "s", false, "")
+	flag.BoolVar(&sha1Flag, "sha1", false, "")
+	flag.BoolVar(&listFlag, "l", false, "")
+	flag.BoolVar(&listFlag, "list", false, "")
 
 	flag.Parse()
+
+	if versionFlag {
+		fmt.Printf("Piker version %s\n", PikerVersion)
+		os.Exit(exitOK)
+	}
 
 }
