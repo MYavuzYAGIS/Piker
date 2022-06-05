7bea671cc57fd1bbef9d55a5a6663d3c09358578
Yavuz <m.yavuzyagis@gmail.com>
2022-06-05 13:25:28 +0300

    Fix some bugs and add some features. This is an AUTOMATED Commit

---
+++ base/base.go
@@ -0,0 +1,42 @@
+package bs4
+
+import (
+	"encoding/base64"
+	"fmt"
+	"log"
+	"strings"
+)
+
+func baseTextDecode(text string) []byte {
+
+	text = strings.ReplaceAll(text, `\/`, `/`)
+	dcd, err := base64.StdEncoding.DecodeString(text)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(dcd))
+	return dcd
+
+}
+
+//
+//func base_text_encode(text) string {
+//	v := validator.New()
+//	text = b64{
+//		Target: "text",
+//	}
+//	err := v.Struct(text)
+//	for _, e := range err.(validator.ValidationErrors) {
+//		fmt.Println(e)
+//	}
+//
+//}
+//
+//func base_file_encode() string {
+//
+//}
+//
+//func base_file_decode() string {
+//
+//}
